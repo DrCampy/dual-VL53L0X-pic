6a491df752504e5a284097f644c4bcdd68895438
Morgan Diepart <morgandiepart@gmail.com>
2020-03-07 20:10:02 +0100

    Misc fixes

--- Code/configuration_bits.c
+++ Code/configuration_bits.c
@@ -1,9 +1,4 @@
-/******************************************************************************/
-/* Files to Include                                                           */
-/******************************************************************************/
-
-
-// PIC24FJ64GA702 Configuration Bit Settings
+// PIC24FJ256GA702 Configuration Bit Settings
 
 // 'C' source line config statements
 
@@ -60,36 +55,4 @@
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 
-#include <xc.h>
-
-/* Device header file */
-#if defined(__XC16__)
-    #include <xc.h>
-#elif defined(__C30__)
-    #if defined(__PIC24E__)
-    	#include <p24Exxxx.h>
-    #elif defined (__PIC24F__)||defined (__PIC24FK__)
-	#include <p24Fxxxx.h>
-    #elif defined(__PIC24H__)
-	#include <p24Hxxxx.h>
-    #endif
-#endif
-
-/******************************************************************************/
-/* Configuration Bits                                                         */
-/*                                                                            */
-/* This is not all available configuration bits for all PIC24 devices.        */
-/* Refer to the PIC24 device specific .h file in the compiler                 */
-/* support\PIC24x\h (x=F,H,E) directory for complete options specific to the  */
-/* selected device.  For additional information about what the hardware       */
-/* configurations mean in terms of device operation, refer to the device      */
-/* datasheet 'Special Features' chapter.                                      */
-/*                                                                            */
-/* A feature of MPLAB X is the 'Generate Source Code to Output' utility in    */
-/* the Configuration Bits window.  Under Window > PIC Memory Views >          */
-/* Configuration Bits, a user controllable configuration bits window is       */
-/* available to Generate Configuration Bits source code which the user can    */
-/* paste into this project.                                                   */
-/******************************************************************************/
-
-/* TODO Fill in your configuration bits from the config bits generator here.  */
+//#include <xc.h>
