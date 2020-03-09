3a3f8d2b13e5b6d0444ed0ed96613b1f3cb6a43e
Morgan Diepart <morgandiepart@gmail.com>
2020-03-09 01:37:47 +0100

    Updated Oscillator configuration code
    Used Microchip MCC generated code

--- Code/system.c
+++ Code/system.c
@@ -15,9 +15,6 @@
     #endif
 #endif
 
-#include <stdint.h>          /* For uint32_t definition */
-#include <stdbool.h>         /* For true/false definition */
-
 #include "system.h"          /* variables/params used by system.c */
 
 /******************************************************************************/
@@ -37,25 +34,47 @@ Refer to the C Compiler for PIC24 MCUs and dsPIC DSCs User Guide in the
 compiler installation directory /doc folder for documentation on the
 __builtin functions. */
 
-/* TODO Add clock switching code if appropriate.  An example stub is below.   */
 void ConfigureOscillator(void)
 {
+    //MCC generated code
 
-#if 0
-
-        /* Disable Watch Dog Timer */
-        RCONbits.SWDTEN = 0;
-
-        /* When clock switch occurs switch to Prim Osc (HS, XT, EC)with PLL */
-        __builtin_write_OSCCONH(0x03);  /* Set OSCCONH for clock switch */
-        __builtin_write_OSCCONL(0x01);  /* Start clock switching */
-        while(OSCCONbits.COSC != 0b011);
-
-        /* Wait for Clock switch to occur */
-        /* Wait for PLL to lock, if PLL is used */
-        /* while(OSCCONbits.LOCK != 1); */
-
-#endif
-        
+    // CPDIV 1:1; PLLEN disabled; DOZE 1:8; RCDIV FRC; DOZEN disabled; ROI disabled;
+    CLKDIV = 0x3000;
+    // STOR disabled; STORPOL Interrupt when STOR is 1; STSIDL disabled; STLPOL Interrupt when STLOCK is 1; STLOCK disabled; STSRC SOSC; STEN disabled; TUN Center frequency;
+    OSCTUN = 0x00;
+    // ROEN disabled; ROSWEN disabled; ROSEL FOSC; ROOUT disabled; ROSIDL disabled; ROSLP disabled;
+    REFOCONL = 0x00;
+    // RODIV 0;
+    REFOCONH = 0x00;
+    // DCOTUN 0;
+    DCOTUN = 0x00;
+    // DCOFSEL 8; DCOEN disabled;
+    DCOCON = 0x700;
+    // DIV 0;
+    OSCDIV = 0x00;
+    // TRIM 0;
+    OSCFDIV = 0x00;
+    // AD1MD enabled; T3MD enabled; T1MD enabled; U2MD enabled; T2MD enabled; U1MD enabled; SPI2MD enabled; SPI1MD enabled; I2C1MD enabled;
+    PMD1 = 0x00;
+    // IC3MD enabled; OC1MD enabled; IC2MD enabled; OC2MD enabled; IC1MD enabled; OC3MD enabled;
+    PMD2 = 0x00;
+    // PMPMD enabled; RTCCMD enabled; CMPMD enabled; CRCMD enabled; I2C2MD enabled;
+    PMD3 = 0x00;
+    // CTMUMD enabled; REFOMD enabled; LVDMD enabled;
+    PMD4 = 0x00;
+    // CCP2MD enabled; CCP1MD enabled; CCP4MD enabled; CCP3MD enabled; CCP5MD enabled;
+    PMD5 = 0x00;
+    // SPI3MD enabled;
+    PMD6 = 0x00;
+    // DMA1MD enabled; DMA0MD enabled;
+    PMD7 = 0x00;
+    // CLC1MD enabled; CLC2MD enabled;
+    PMD8 = 0x00;
+    // CF no clock failure; NOSC FRCPLL; SOSCEN disabled; POSCEN disabled; CLKLOCK unlocked; OSWEN Switch is Complete; IOLOCK not-active;
+    __builtin_write_OSCCONH((uint8_t) (0x01));
+    __builtin_write_OSCCONL((uint8_t) (0x01));
+    // Wait for Clock switch to occur
+    while (OSCCONbits.OSWEN != 0);
+    while (OSCCONbits.LOCK != 1);
 }
 
