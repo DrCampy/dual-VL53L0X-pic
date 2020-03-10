7801d6fff0e973ac39673c0acc531eca49e37068
Morgan Diepart <morgandiepart@gmail.com>
2020-03-10 18:58:23 +0100

    OSB2 pin is general purpose output

--- Code/configuration_bits.c
+++ Code/configuration_bits.c
@@ -24,7 +24,7 @@
 
 // FOSC
 #pragma config POSCMD = NONE    //Primary Oscillator Mode Select bits->Primary Oscillator disabled
-#pragma config OSCIOFCN = OFF    //OSC2 Pin Function bit->OSC2 is clock output
+#pragma config OSCIOFCN = ON    //OSC2 Pin Function bit->OSC2 is general purpose digital I/O pin
 #pragma config SOSCSEL = OFF    //SOSC Power Selection Configuration bits->Digital (SCLKI) mode
 #pragma config PLLSS = PLL_FRC    //PLL Secondary Selection Configuration bit->PLL is fed by the on-chip Fast RC (FRC) oscillator
 #pragma config IOL1WAY = ON    //Peripheral pin select configuration bit->Allow only one reconfiguration
